5caf5d08f1cd687de0e8232a96db62a00a4e118e
Nicolas Jooris <njooris@z1r4p2.42lyon.fr>
2025-12-18 15:53:38 +0100

    cpp03

---
+++ ex00/includes/ClapTrap.hpp
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: njooris <njooris@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/18 13:05:26 by njooris           #+#    #+#             */
+/*   Updated: 2025/12/18 15:31:21 by njooris          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#include <string>
+
+class	ClapTrap
+{
+	private:
+		std::string	_name;
+		size_t		_point_health;
+		size_t		_point_energy;
+		size_t		_attack_damage;
+
+	public:
+		ClapTrap(std::string name);
+		ClapTrap(const ClapTrap &claptrap);
+		~ClapTrap(void);
+		
+		ClapTrap	&operator=(const ClapTrap &claptrap);
+
+		const std::string	&get_name() const;
+		const size_t		&get_point_health() const;
+		const size_t		&get_point_energy() const;
+		const size_t		&get_attack_damage() const;
+
+		bool	can_do_something(void) const;
+		void	attack(const std::string &target) const;
+		void	takeDamage(const unsigned int amount);
+		void	beRepaired(const unsigned int amount);
+};
+
+#endif
---
+++ ex00/srcs/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: njooris <njooris@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/18 15:47:50 by njooris           #+#    #+#             */
+/*   Updated: 2025/12/18 15:50:39 by njooris          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "ClapTrap.hpp"
+
+int	main(void)
+{
+	ClapTrap	tom("Tom");
+	ClapTrap	lucas("Lucas");
+
+	lucas.attack("Tom");
+	tom.attack("Lucas");
+
+	lucas.beRepaired(1);
+	tom.beRepaired(1);
+	
+	lucas.takeDamage(5);
+	tom.takeDamage(5);
+	
+	return (0);
+}
