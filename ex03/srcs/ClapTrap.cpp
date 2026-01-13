77829196d420c4faabbbdab5d5a29d941b906218
Nicolas Jooris <njooris@z1r5p2.42lyon.fr>
2026-01-13 13:29:55 +0100

    patch space msg

--- ex03/srcs/ClapTrap.cpp
+++ ex03/srcs/ClapTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: njooris <njooris@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/18 14:46:12 by njooris           #+#    #+#             */
-/*   Updated: 2026/01/13 12:48:22 by njooris          ###   ########.fr       */
+/*   Updated: 2026/01/13 13:29:44 by njooris          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -84,7 +84,7 @@ void	ClapTrap::attack(const std::string& target) const
 {
 	if (can_do_something())
 		return ;
-	std::cout << "CalpTrap" << _name << " attacks " << target << ", causing " << _attack_damage << " points of damage!" << std::endl;
+	std::cout << "CalpTrap " << _name << " attacks " << target << ", causing " << _attack_damage << " points of damage!" << std::endl;
 }
 
 void	ClapTrap::takeDamage(unsigned int amount)
@@ -99,7 +99,7 @@ void	ClapTrap::takeDamage(unsigned int amount)
 	{
 		this->_point_health -= amount;
 	}
-	std::cout << "CalpTrap" << _name << " take damages and lose " << amount << "points of health" << std::endl;
+	std::cout << "CalpTrap " << _name << " take damages and lose " << amount << "points of health" << std::endl;
 	_point_energy--;
 }
 
@@ -115,6 +115,6 @@ void	ClapTrap::beRepaired(unsigned int amount)
 	{
 		this->_point_health += amount;
 	}
-	std::cout << "CalpTrap" << _name << "repared it self and regain " << amount << "points of health" << std::endl;
+	std::cout << "CalpTrap " << _name << " repared it self and regain " << amount << "points of health" << std::endl;
 	_point_energy--;
 }
